d2619577ae9b9b24af1f0e7280efa002731df280
kcsc2217 <k12002@nate.com>
2024-02-06 18:25:15 +0900

    게시판 도메인 개발

---
+++ src/main/java/board/domain/Board.java
@@ -0,0 +1,26 @@
+package board.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class Board {
+
+    private Long id;
+    private String sub;
+    private String content;
+    private Date date;
+    private int count;
+
+    public Board(String sub, String content, Date date, int count) {
+        this.sub = sub;
+        this.content = content;
+        this.date = date;
+        this.count = count;
+    }
+
+
+}
---
+++ src/main/java/board/repository/BoardRepository.java
@@ -0,0 +1,25 @@
+package board.repository;
+
+import board.domain.Board;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoardRepository {
+
+    Board save(Board board);
+    Optional<Board> findById(Long id);
+    List<Board> findByAll();
+
+    void upDate(Long id, Board board);
+
+    void deleteBoard(Long id);
+
+
+
+
+
+
+
+
+}
